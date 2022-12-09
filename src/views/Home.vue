d2c27a54bc16b0faa18a4f9ef0a4dbee85913533
paresiqbal <thecrowdel@gmail.com>
2022-12-09 08:20:41 +0700

    Update Card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,70 +5,10 @@
     </div>
 
     <div class="cardContainer">
-      <div class="card">
+      <div class="card" v-for="card in cards" :key="card.key">
         <div class="picture">
-          <img src="../assets/image/front end.png" />
-          <p>Front End</p>
-        </div>
-        <div class="icons">
-          <a href=""
-            ><font-awesome-icon icon="fa-solid fa-chevron-right fa-2xl"
-          /></a>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="picture">
-          <img src="../assets/image/backend.png" />
-          <p>Back End</p>
-        </div>
-        <div class="icons">
-          <a href=""
-            ><font-awesome-icon icon="fa-solid fa-chevron-right fa-2xl"
-          /></a>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="picture">
-          <img src="../assets/image/backend.png" />
-          <p>Back End</p>
-        </div>
-        <div class="icons">
-          <a href=""
-            ><font-awesome-icon icon="fa-solid fa-chevron-right fa-2xl"
-          /></a>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="picture">
-          <img src="../assets/image/backend.png" />
-          <p>Back End</p>
-        </div>
-        <div class="icons">
-          <a href=""
-            ><font-awesome-icon icon="fa-solid fa-chevron-right fa-2xl"
-          /></a>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="picture">
-          <img src="../assets/image/backend.png" />
-          <p>Back End</p>
-        </div>
-        <div class="icons">
-          <a href=""
-            ><font-awesome-icon icon="fa-solid fa-chevron-right fa-2xl"
-          /></a>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="picture">
-          <img src="../assets/image/backend.png" />
-          <p>Back End</p>
+          <img :src="card.images" alt="backend.png" />
+          <p>{{ card.judul }}</p>
         </div>
         <div class="icons">
           <a href=""
@@ -82,17 +22,69 @@
       <button>Lihat Semua</button>
     </div>
 
-    <figure v-for="card in cards">
-      <figcaption>{{ card.judul }}</figcaption>
-      <img src="{{ card.image }}" />
-    </figure>
+    <!-- <figure v-for="card in cards" :key="card.key">
+      <p>{{ card.judul }}</p>
+      <img :src="card.images" alt="backend.png" />
+    </figure> -->
   </div>
 </template>
 
 <script setup>
-const cards = [
-  { judul: "Front End", images: "../../assets/image/backend.png" },
-];
+import { ref } from "vue";
+
+const cards = ref([
+  { key: 1, judul: "Frontend", images: "src/assets/image/frontend.png" },
+  {
+    key: 2,
+    judul: "Backend",
+    images: "src/assets/image/backend.png",
+  },
+  {
+    key: 3,
+    judul: "UI/UX",
+    images: "src/assets/image/uiux.png",
+  },
+  {
+    key: 4,
+    judul: "Product Manager",
+    images: "src/assets/image/produkmanager.png",
+  },
+  {
+    key: 5,
+    judul: "System Analist",
+    images: "src/assets/image/systemanalis.png",
+  },
+  {
+    key: 6,
+    judul: "Quality Asurance",
+    images: "src/assets/image/qualityasurance.png",
+  },
+  {
+    key: 7,
+    judul: "Digital Marketing",
+    images: "src/assets/image/digitalmarketing.png",
+  },
+  {
+    key: 8,
+    judul: "Business Development",
+    images: "src/assets/image/businesdevelopment.png",
+  },
+  {
+    key: 9,
+    judul: "Content Creator",
+    images: "src/assets/image/contentcreator.png",
+  },
+  {
+    key: 10,
+    judul: "Graphic Design",
+    images: "src/assets/image/graphicdesign.png",
+  },
+  {
+    key: 11,
+    judul: "3D",
+    images: "src/assets/image/3dmodeling.png",
+  },
+]);
 </script>
 
 <style scoped>
@@ -116,25 +108,28 @@ const cards = [
   gap: 30px;
 }
 .card {
-  max-width: 22rem;
+  /* max-width: 22rem; */
   height: 170px;
   border-radius: 15px;
   display: flex;
   align-items: center;
   background-color: #dddd;
-  gap: 160px;
   padding-left: 35px;
   padding-right: 20px;
 }
+
+.picture {
+  width: 260px;
+}
 .picture img {
-  height: 100px;
   max-width: 150px;
+  height: 90px;
   border-radius: 5px;
 }
 
 .picture p {
   font-weight: 700;
-  font-size: 25px;
+  font-size: 22px;
   padding-top: 5px;
 }
 
